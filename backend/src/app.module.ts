74ecdba52a9f4376cfaec7ba5bc85661a9dea525
zaid <gabozaidinde@gmail.com>
2025-12-10 01:04:07 -0600

    feat: integrate Amadeus API with service and module setup, add environment variables, and update configurations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AmadeusModule } from './amadeus/amadeus.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AmadeusModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/types/amadeus.d.ts
@@ -0,0 +1,18 @@
+export interface AmadeusConfig {
+  AMADEUS_CLIENT_ID: string;
+  AMADEUS_CLIENT_SECRET: string;
+  AMADEUS_API_URL: string;
+  AUTH_URL_AMADEUS: string;
+}
+
+export interface AmadeusOAuthResponse {
+  type: 'amadeusOAuth2Token';
+  username: string;
+  application_name: string;
+  client_id: string;
+  token_type: 'Bearer';
+  access_token: string;
+  expires_in: number;
+  state: 'approved' | 'expired';
+  scope: string;
+}
